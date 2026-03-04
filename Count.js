f84b4673970e21dde06ed3559b203205af6de3be
Sireesha <anilch03@gmail.com>
2026-03-04 22:38:51 +0530

    count js updated

--- Count.js
+++ Count.js
@@ -1,25 +1,25 @@
 const count = document.getElementById("count");
-const decrementBtn = document.getElementById("decrementBtn");
-const resetBtn = document.getElementById("resetBtn");
-const incrementBtn = document.getElementById("incrementBtn");
+const decrementBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+const incrementBtn = document.getElementById("increase");
 let currentCount = 0;
 // Update the count display
 function updateCount() {
     count.textContent = currentCount;
 }  
 // decrement Event Listener
-decrementBtn.addEventListener("click", function() {
-    currentCount--;
+decrementBtn.addEventListener("click",function() {
+    currentCount=currentCount-5;
     updateCount();
 });
 // reset Event Listener
-resetBtn.addEventListener("click", function() {
+resetBtn.addEventListener("click",function() {
     currentCount = 0;
     updateCount();
 });
 // increment Event Listener
-incrementBtn.addEventListener("click", function() {
-    currentCount++;
+incrementBtn.addEventListener("click",function() {
+    currentCount=currentCount+5;
     updateCount();
 });
 updateCount();
